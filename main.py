7f60b586a454519da22c08b70fe2cc14c3b883be
OrangeUnivers <118116610+OrangeUnivers@users.noreply.github.com>
2025-01-13 15:46:54 +0100

    asdaeds

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Start")
+mapDir = open("map/mapsLoad.txt", "r")
+maps = mapDir.readlines()
+print(maps)
+if len(maps) == 0:
+    raise Exception("There is no map to load!")
+
+for index in maps:
+    print(index)
